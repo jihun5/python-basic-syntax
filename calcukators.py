dac273accd43fecf56d0a31aa185c0115d710c87
juhun5 <wlgns6924@naver.com>
2023-05-22 17:24:12 +0900

    수정

---
+++ calcukators.py
@@ -0,0 +1,25 @@
+# ctrl + f5 = Run
+a = 3
+b = 4
+# 뎃셈 : +, 빼기 : -, 곱하기 : *, 나누기 : /, 몫 : //, 나머지 : %
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+
+num1 = 10
+num2 = 20
+answer = num1+num2
+print(answer)
+
+# 자료의 형변환
+# 숫자 -> 문자, 실수 -> 정수
+a = 10
+b = 20
+# 결과값이 1020이 나오도록 덧셈을 하여라
+print(str(a)+str(b))
+c = 2333.33333
+# c의 소수부분을 잘라서 출력하라
+print(int(c))
