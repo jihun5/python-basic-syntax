9ff38b31be0a4482dbb06d89458ccd237ac6d37d
juhun5 <wlgns6924@naver.com>
2023-05-23 16:36:26 +0900

    new

--- variables.py
+++ variables.py
@@ -31,114 +31,7 @@ c = 2333.33333
 # c의 소수부분을 잘라서 출력하라
 print(int(c))
 
-# 문자자료형
-# 문자는 ""쌍따옴표 또는 ''따옴표로 표현을 한다.
-# 'a' 문자에 매듭
-
-# 'a'라는 문자를 변수에 저장하게 되면, 메모리상에 어떤 숫자값으로 저장될까?
-# 아스키 코드라는 문자와 매칭되는 테이블을 사용하여 약속된 숫자값으로 저장
-# 현대에는 아스키코드의 표현범위의 한계로 인해, utf-8을 표준으로 사용
-x = 'A' # a = 97(컴퓨터 메모리상), ord = 내장된 함수값
-print(ord(x))
-y = 'a'
-print(ord(y))
-a = 'he1ll2oWO1rld' # 숫자로 입력하기, 각각 아스키 코드 대입하여 계산가능
-
-# multi line으로 문자열을 표현하고 싶으면, 쌍따옴표 3개를 사용하거나 홀따옴표 3개를 사용하면 된다. 
-a = '''hello
-
-# world'''
-# escape문을 활용한 줄바꿈
-# 이스케이프문이란 \n 또는 \t 등의 특수기호를 말한다.
-# \n : 줄바꿈, \t : tap키
-# \의 또 다른 활용 : 특수문자를 있는 그대로 표현하는 역할
-# "쌍따옴표(")는 파이썬에서 문자를 의미한다."라는 문구를 출력해보자.
-print("쌍따옴표(\")는 파이썬에서 문자를 의미한다.") 
-a = "hello \n world"
-print(a)
-
-# 그렇다면 python's easy라는 문구를 출력해보자. 
-# a = ''python's easy' 중간에 홑따옴표가 인식이되어서 안되는 경우가 있을 수 있어 쌍따옴표를 사용
-print("python's easy")
-
-# 문자열 더하기, 곱하기
-# a라는 변수에 python이라는 문자열을 담고, b라는 변수에는 is fun이라는 문자열을 담는다.
-# 그리고 c라는 변수에 두 문자열을 더한 겂을 넣어 c를 출력
-a = 'python '
-b = " is fun"
-c = a + b
-print(c)
-# python python is fun이라는 문자열을 c에 담아 출력 
-c = a *2 + b
-print(c)
-
-# 파이썬에서 인덱스란, 기본적으로 특정위치를 가리키는 용도로 사용
-# 변수명[인덱스 번호] - 인덱스 번호에 위치한 값을 반환 
-# # 인덱스의 사용방법은 원하는 대상[숫자값]
-# A = "python is fun" print(a[0]) >> p
-# 프로그래밍에서는 대부분의 순서는 0부터 시작(0,1,2,3...의 순서, 공백포함)
-# 문자 h를 출력하라
-a = "python's fun"
-print(a[3])
-# 문자열의 마지막 문자를 출력해보자.
-a = "python's fun python's fun python's fun"
-print(a[-1])
-# 문자열의 길이를 구하는 함수는 len
-print(len(a))
-print(a[len(a)-1])
-
-# 문자열의 슬라이싱
-# 슬라이싱이란 문자열을 잘라내는 것을 의미
-# 대상변수[x:y] : x이상 y 미만의 index를 가진 문자열을 잘라낸다.
-a = "python is fun"
-# python만 잘라내기
-b = a[0:6]
-print(b)
-# x, y 자리에 값이 없으면 처음부터 끝까지를 의미
-# 6번째 문자부터 끝까지 잘라내서 변수 b에 담아 출력 
-b = a[6:]
-print(b)
-# a[x:y:z] 여기에서 z는 z-1 개씩 건너뛰고
-# 2번째 이상 7번째 미만 문자열 중에 1개씩 건너뛰고 b에 담아 출력
-b = a[2:7:2]
-print(b)
-# 처음부터끝까지 3개씩 뛰고
-print(a[::3])
-# 연습문제 A = '20220505children's_day' 슬라이싱을 이용하여 date 라는 변수에 날짜
-# day라는 날에 children's_day를 각각 출력하시오
-A = '20220505children\'s_day'
-date = A[:8]
-day = A[8:]
-print(date)
-print(day)
-
-# 문자열 포맷팅이란 문자열 중간에 특정 문자(또는 숫자 등)를 삽입하는 방식.
-# %s : 문자열, %d : 정수, %f : 실수
-# 포맷팅을 왜 쓰는가? 
-# 1. 문자열을 직접 삽입하면 1회성으로 coding할 수 밖에 없지만 포맷팅을 사용하면 변수값을 삽입할 수 있다.
-# 2. 따옴표를 여러번 안써도 된다.
-# language = input("좋아하는 언어를 입력하시오:")
-# times = input("그 언어를 몇 번이나 공부하셨나요?:")
-# a = "I studied %s very hard %d times" % (language, int(times)) # %s자리에 %"a" a를 추가할 것 , 
-# 2역시 같은 원리 같이 사용할 경우 앞과 같은 모습 language 입력 가능 %d자리에 역시 마찬가지로 변수 사용가능
-# %d 가 숫자형이기 때문에 times 자리에 int를 사용해야 오류가 나지 않음 
-# print(a)
-# # 아래와 같이 코딩하면 따옴표로 열고 닫고 너무 많이해서 귀찮다.
-# a = "I studied " + language + "very hard" + str(times) + "times"
-# print(a)
-
-# my age is %d, and weight is %f kg
-# 나이가 몇살이신가요?라고 해서 나이를 받고
-# 몸무게가 몇 킬로그램 이신가요?라고 해서 weight 받고 
-# 위 문자열을 포맷팅을 통해 사용자의 입력값에 따라 달라지도록 만들고 그 결과값을 출력하라
-# age = int(input("나이가 몇살이신가요?:"))
-# weight = float(input("몸무게가 몇Kg 이신가요?:"))
-# result = "my age is %d my 몸무게는? %f" %(age, weight)
-# print(result)
-# 변수 a에 2022 변수 b에 호랑이를 선언하고 
-# 결과 : 올해는 2022년 %s의 해 입니다. %
-
-# 문자열 관련 주요 함수
+# 문자열 관련 주요 함수 ****
 # count : 대상 문자열에 지정한 문자가 몇개가 있는지 출력하는 함수
 # a = "python"
 # print(a.count('o'))
@@ -175,25 +68,41 @@ print(a.replace('python', 'java'))
 
 # 문자열 나누기(split)
 # 공백을 기준으로 문자를 자르는 함수 : split()
-a = "I studied python"
-b = a.split() 
-print(b)
-# 공백을 넣는 것(" " 빈칸하나 공백만큼 자르기 남은 빈칸은 그대로 출력)과 넣지 않는 것(비어있는 값을 버리기)
-a = "I   studied   python"
-b = a.split(" ")
-c = a.split()
-print(b)
-print(c)
+# a = "I studied python"
+# b = a.split() 
+# print(b)
+# # 공백을 넣는 것(" " 빈칸하나 공백만큼 자르기 남은 빈칸은 그대로 출력)과 넣지 않는 것(비어있는 값을 버리기)
+# a = "I   studied   python"
+# b = a.split(" ")
+# c = a.split()
+# print(b)
+# print(c)
 
-a = "I:studied:python"
-b = a.split(":") #원하는 문자열로 자를 수 있다. 
-print(b)
+# a = "I:studied:python"
+# b = a.split(":") #원하는 문자열로 자를 수 있다. 
+# print(b)
 
-# 아래와 같은 2차 방정식을 파이썬 수식어로 코딩하고 y의 결과를 출력하라
-# Y = 2.5 X x제곱 + 3.3 X x +6(x는2)
-# 출력화면 : 2차 방정식 결과 = 22.6
-x = int(input("x값을 입력해주세요:"))
-y = 2.5 * pow(x, 2) + 3.3 * x + 6
-print(y) 
+# # 아래와 같은 2차 방정식을 파이썬 수식어로 코딩하고 y의 결과를 출력하라
+# # Y = 2.5 X x제곱 + 3.3 X x +6(x는2)
+# # 출력화면 : 2차 방정식 결과 = 22.6
+# x = int(input("x값을 입력해주세요:"))
+# y = 2.5 * pow(x, 2) + 3.3 * x + 6
+# print(y) 
 
-print("날씨가")
+# 연습문제 3개의 단어를 입력받아 첫글자 추출후 약자를 추출
+#  조건 1> 각 단여의 변수 word1 ...3
+#  조건 2> 입력과 출력 구분선: 문자열 연산
+#  조건 3> 각변수의 첫 단어만 추출하여 변수 저장(index)
+
+# 출력화면 에씨
+# 첫번째 단어 : korea
+# 두번째 단어 : Baseball
+# 세번째 단어 : orag
+# ======================
+# 약자 : KBO
+
+word1 = input("첫번째 단어 :")
+word2 = input("두번째 단어 :")
+word3 = input("세번째 단어 :")
+print("="*20)
+result = print("약자:", word1[0]+word2[0]+word3[0])
