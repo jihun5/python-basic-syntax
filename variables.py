7e67ea9b412f620240e87795b291de79187538a8
juhun5 <wlgns6924@naver.com>
2023-05-23 16:09:36 +0900

    test commit

--- variables.py
+++ variables.py
@@ -117,8 +117,81 @@ print(day)
 # 포맷팅을 왜 쓰는가? 
 # 1. 문자열을 직접 삽입하면 1회성으로 coding할 수 밖에 없지만 포맷팅을 사용하면 변수값을 삽입할 수 있다.
 # 2. 따옴표를 여러번 안써도 된다.
-language = input("좋아하는 언어를 입력하시오:")
-times = input("그 언어를 몇 번이나 공부하셨나요?:")
-a = "I studied %s very hard %d times" % (language, int(times)) # %s자리에 %"a" a를 추가할 것 , 
-# 2역시 같은 원리 같이 사용할 경우 앞과 같은 모습,"python" 자리에 변수 language 입력 가능 %d자리에 역시 마찬가지로 변수 사용가능
-print(a)
+# language = input("좋아하는 언어를 입력하시오:")
+# times = input("그 언어를 몇 번이나 공부하셨나요?:")
+# a = "I studied %s very hard %d times" % (language, int(times)) # %s자리에 %"a" a를 추가할 것 , 
+# 2역시 같은 원리 같이 사용할 경우 앞과 같은 모습 language 입력 가능 %d자리에 역시 마찬가지로 변수 사용가능
+# %d 가 숫자형이기 때문에 times 자리에 int를 사용해야 오류가 나지 않음 
+# print(a)
+# # 아래와 같이 코딩하면 따옴표로 열고 닫고 너무 많이해서 귀찮다.
+# a = "I studied " + language + "very hard" + str(times) + "times"
+# print(a)
+
+# my age is %d, and weight is %f kg
+# 나이가 몇살이신가요?라고 해서 나이를 받고
+# 몸무게가 몇 킬로그램 이신가요?라고 해서 weight 받고 
+# 위 문자열을 포맷팅을 통해 사용자의 입력값에 따라 달라지도록 만들고 그 결과값을 출력하라
+# age = int(input("나이가 몇살이신가요?:"))
+# weight = float(input("몸무게가 몇Kg 이신가요?:"))
+# result = "my age is %d my 몸무게는? %f" %(age, weight)
+# print(result)
+# 변수 a에 2022 변수 b에 호랑이를 선언하고 
+# 결과 : 올해는 2022년 %s의 해 입니다. %
+
+# 문자열 관련 주요 함수
+# count : 대상 문자열에 지정한 문자가 몇개가 있는지 출력하는 함수
+# a = "python"
+# print(a.count('o'))
+# # find : 대상 문자열에서 지정한 문자가 몇 번 index에 있는 지 찾는 함수
+# print(a.find('o'))
+# print(a.index('o'))
+
+# whatyouwant = input("아무런 문자나 입력해주세요:")
+# search = input("찾고자 하는 문자 1개만 입력해 주세요")
+# result = whatyouwant.find(search)
+# if result == -1:
+#     print("찾고자 하는 값이 없습니다.")
+# else:
+#     print("요청하신 문자는 %d 번째에 있습니다" % result)
+
+# 대소문자 변경 : upper() / lower()
+a = "hello"
+a1 = a.upper()
+print(a1)
+b = "HELLO"
+print(b.lower())
+
+# 문자열 양쪽 공백을 없애는 함수 : strip()
+a = "   hello word   "
+print(a.strip())
+
+# myid = (input("id를 입력해주세요")).strip()
+# mypw = input("비밀번호를 입력해주세요")
+# print("id는 " +myid + " 이고" + "비밀번호는" +mypw)
+
+# 문자열 대체 : replace()
+a = "I studied python"
+print(a.replace('python', 'java'))
+
+# 문자열 나누기(split)
+# 공백을 기준으로 문자를 자르는 함수 : split()
+a = "I studied python"
+b = a.split() 
+print(b)
+# 공백을 넣는 것(" " 빈칸하나 공백만큼 자르기 남은 빈칸은 그대로 출력)과 넣지 않는 것(비어있는 값을 버리기)
+a = "I   studied   python"
+b = a.split(" ")
+c = a.split()
+print(b)
+print(c)
+
+a = "I:studied:python"
+b = a.split(":") #원하는 문자열로 자를 수 있다. 
+print(b)
+
+# 아래와 같은 2차 방정식을 파이썬 수식어로 코딩하고 y의 결과를 출력하라
+# Y = 2.5 X x제곱 + 3.3 X x +6(x는2)
+# 출력화면 : 2차 방정식 결과 = 22.6
+x = int(input("x값을 입력해주세요:"))
+y = 2.5 * pow(x, 2) + 3.3 * x + 6
+print(y)
