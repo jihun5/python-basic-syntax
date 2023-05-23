ce84a3b407c6685e9f96601f8bee079ceec8c2ea
juhun5 <wlgns6924@naver.com>
2023-05-23 14:07:03 +0900

    fix

--- calcukators.py
+++ calcukators.py
@@ -14,3 +14,13 @@ num2 = 20
 answer = num1+num2
 print(answer)
 
+# 2일차
+# 제곱과 제곱근
+# 2의 10제곱을 출력하라
+print(2**10) 
+print(pow(2,10)) # pow 제곱 내장함수
+import math
+print(math.pow(2,10))
+# 1024의 제곱근을 구하라
+# 제곱근은 math라는 라이브러리를 import해야한다
+print(math.sqrt(1024))
