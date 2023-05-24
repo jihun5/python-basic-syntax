5bfc8d5ea577cbb2c89394b9df46be08cf71ef5d
juhun5 <wlgns6924@naver.com>
2023-05-24 10:35:03 +0900

    new

---
+++ 08.for.py
@@ -0,0 +1,16 @@
+# for문의 기본 구조
+# for 변수 in 반복가능한자료형
+#     실행문
+lista = [1,2,3,4,5,6,7,8,9,10]
+for a in lista:
+    print(a) # 10번의 반복을 하게 됨
+
+# range 문법
+for a in range(1, 101): # a, a-1의 범위 1부터 100까지는 1, 101
+    print(a, end=' ')
+
+# listb = ["a","b","c","d"]
+# for b in range(0, len(listb)):
+#     print(listb[b]) 파이썬 외에 언어 기본문법
+
+
