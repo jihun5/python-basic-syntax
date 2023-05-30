945f8317cf21ad60aada8fa555693fc746d2d589
juhun5 <wlgns6924@naver.com>
2023-05-30 11:28:33 +0900

    new

--- 17.function.py
+++ 17.function.py
@@ -168,6 +168,66 @@ def cal(a,b,c ='plus'):
 print(cal(1,2)) # 기본 값으로 plus를 가지고 있기때문에 1+2가 출력됨
 print(cal(1,2, "minus"))# 다른 조건이 들어오면 조건이 출력됨 1-2가 출력
 
+# 매개변수의 순서를 안맞추고도 원하는 매개변수의 자리에 값을 넣어
+# 함수를 호출하는 방법
+def whoareyou(name, age, gender):
+    print(f"제이름은 {name}이고, 나이는{age}, 성별은{gender}입니다.")
+whoareyou(age=19,name="홍길동",gender="남자")
+
 # 파이썬에서 default값 세팅하는 대표적인 예시가 print함수
-print("hello")
+# print 2개를 사용해서 줄바꿈 없이 hello world라고 출력이 되도록 만들어보자
+print("hello", end=" ")
 print("world")
+
+# 지역변수와 전역변수
+# 지역변수 : 함수내에서의 변수, 함수내에서만 유효
+# 전역변수 : 함수밖에서의 변수, 함수내에서도 인식가능
+a = 100
+c = 200
+def sum(a, b):      #지역변수, a=100이랑 전혀상관이 없음, 함수 내에서 선언한 a=10이 우선적으로 인식
+    # 함수내에서 함수밖의 전역변수를 사용하려면 global 키워드 사용
+    # global c # 밖에있는 c값을 가져오기
+    result = a+b+c    #지역변수
+    return result   #지역변수
+result = sum(10,20)
+print(result)  # 함수 내의 result라는 변수는 함수밖에서는 인식불가
+# 우리가 result 프린트 할 수 있었던 것은, 함수내에서 어떤값을 return을 해줬기 때문
+print(a)
+
+lista = [10,20,30,40]
+# for문마다 같은 변수를 사용하는 것은 전역변수이기는 하지만,
+# 재할당을해서 사용하는 것이므로 어차피 reset되서 문제가 되지 않는다
+# for a in range(lista): #틀림 list는 integer 안됨 셀수가 없음
+for a in range(len(lista)):    
+    print(a)
+for a in range(len(lista)):    
+    print(a)
+print(a)
+
+# 이중 for문을 사용할 경우 문제가 될 여지가 있다.
+# 다중 for문을 쓸때는 상위의 for문의 변수를 잃어버리게 되므로, 다른 변수명을 사용
+for a in range(len(lista)):    
+    for b in range(len(lista)):    # 같은 변수 a의 값을 b로 변경
+        print(a)
+
+# 함수 내에서 전역변수에 영향을 끼치는 방법 : global
+# 기본적으로 함수 내에서 함수밖에 전역변수를 수정할 수 없다.
+# 그 이유는 저장되는 메모리 위치가 다르기때문, 
+# 전역함수(데이터 영역)/객체(힙영역)/지역변수(스택영역) 데이터 영역의 값을 조회가능하지만 스택영역에서 값을 바꾸는 것은 불가능하다
+# 힙영역에서 스택영역은 조회 및 수정이 가능하다.
+result = 0
+def sum(a):
+    global result
+    result += a
+    return result
+value = sum(10)
+print(value)
+
+# 객체는 힙메모리 영역에 저장되는데, 함수내에서도 접근하여 추가/ 수정이 가능하다.
+# 스택영역에 있는 지역변수는 함수가 끝나면 휘발되지만, 힙메모리는 휘발되지 않는다.
+# 원본의 주소를 알아야 lista의 append가 가능
+lista = [2,3,4]
+def appendTest(input1, input2):
+    input1.append(input2)
+appendTest(lista, 5)
+print(lista)
