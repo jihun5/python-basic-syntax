38fd99e266d34aee62895e93529d8f28b565574b
juhun5 <wlgns6924@naver.com>
2023-06-05 18:00:36 +0900

    new

--- ----pratice.py
+++ ----pratice.py
@@ -89,3 +89,50 @@ arr길이가 짝수라면 arr 모든 홀수 인덱스 위치에 n을 더한 배�
      # (짝수항에다가 더한 값들만 출력이 됐어)
 # print(a1) 
 
+'''     
+cipher를 주고 받는데 code번째의 글짜만 암호다
+출력하라
+'''
+# cipher = "dfjardstddetckdaccccdegk"
+# code = 4
+# # # result = "attack"
+# # print(cipher[::code])
+
+# answer = ""
+# for i in range(code, len(cipher)+1): # 4에서 24라고 가정하고
+#      if i % code == 0: # 4~8~12 번째만 출력
+#           answer += cipher[i-1] # i는 4의 배수지만 숫자가 0부터 시작이니까 3, 7을 출력
+
+# isupper() - 해당 문자열이 대문자인지 판단
+# my_string = "cccCCC"
+# a = my_string.swapcase()
+# print(a)
+
+'''   
+직육면체 모양의 박스하나를 가지고 있고
+이 상자에 최대한 많이 채우고 싶어 주사위를
+상자의 가로 세로 높이가 저장되어있는 배열 box
+주사위 모서리 개수가 n으로 주어졌을 때
+상자에 들어갈 수 있는 주사위에 최대 개수
+'''
+#예를 들어 가로가 10이면 3개에다가
+#세로가 8이면 2개 
+# 가로 곱하기 세로 만큼 나오고 6개
+#높이 까지 곱해서 12 
+# 가로 나눈 몫 * 세로 나눈 몫 * 높이 나눈 몫 = 상자에 담길 수 있는 주사위
+# box = [10,8,6]
+# n = 3
+
+
+# a1 = []
+# a = (box[0]//n)
+# b = (box[1]//n)
+# c = (box[2]//n)
+# answer = a*b*c
+# print(answer)
+
+# ineq = "<"
+# eq = "="
+# n = 20
+# m = 50
+# # ineq, eq :  n <= m 참이면 1 
