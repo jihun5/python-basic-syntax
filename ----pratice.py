4c296c111ada4488714c2f65b97d2cedc27e6ad6
juhun5 <wlgns6924@naver.com>
2023-06-12 15:10:55 +0900

    new

--- ----pratice.py
+++ ----pratice.py
@@ -1,99 +1,3 @@
-
-'''  
-문자열을 다 합친것을 꼬리 문자열이라고 부르는디
-특정 문자열을 제외하고 출력하는 것을 구하려고한다
-'''
-# str_list = ["abc", "def", "ghi"]
-# ex = "ef"
-# answer = ""
-# for a in str_list: # abc, def, ghi
-#     if not ex in a: #만약 ex가 abc, def, ghi쪽에 없으면
-#         answer += a # a를 answer에다가 추가하라 -가 안되는 이유는 문자열끼리 -는 안되니까
-# print(answer)
-
-'''
-빈배열 x가 있고 양의 정수 배열 arr가 매개 변로 주어질때
-arr앞에서부터 차례대로 원소를 원소 a가 x의 맨뒤에 a번추가 하는 일을
-반복적으로 한뒤 배열 x를 리턴하라
-'''
-# arr = [5,1,4]
-# # answer = [5,5,5,5,5,1,4,4,4,4]
-# # # a = arr[0]*5
-# # # print(a)
-# # for a in arr:
-# #     b = arr.append(a)
-# # print(b)
-
-# answer = []
-# for i in range(len(arr)): # 012
-#     answer.append(i)
-
-# mySting = "AABAA"
-# A = mySting.replace("A", "B")
-# B = mySting.replace("B", "A")
-# for a in mySting:
-#     "A" == "B" and "B" == "A"
-# print(a)
-
-# '''
-# 연산 *은 두 정수에 대한 연산으로 두 정수를 붙여쓴 값을 반환한다
-# '''
-# #글자로 나타태고 합쳐버리고
-# # mySting = "abstract alagebra"
-# # a = mySting.lower()
-# # print(a)
-# # a.replace("a", "A")
-# # print(a.replace("a", "A"))
-# a= "ccs"
-# a.upper
-# import requests
-# import json
-# url = "https://api.binance.com/api/v3/ticker/24hr"
-# response = requests.get(url)
-# data_json = json.loads(response.text)
-# print(data_json[0])
-# # 출력 결과
-# # "symbol" : "BTCUSDT", "lastPrice" : xxxxx(가격)
-# for a in data_json:
-#     if a['symbol'] == "BTCUSDT":
-#         print(f"{a['symbol']}코인의 price는 {a['lastPrice']}입니다.")
-# # print(f"{a['symbol']}코인의 price는 {a['lastPrice']} 입니다.")
-# # print가 아니라, 값을 변수에 담아서, DB에 저장
-# return my_string.split(" ")
-
-'''
-arr과 정수 n arr의 길이가 홀수면 arr의 짝수 인덱스 위치에 n을 더하고
-arr길이가 짝수라면 arr 모든 홀수 인덱스 위치에 n을 더한 배열을 출력하라
-
-'''
-
-# arr = [49, 12, 100, 276, 33]
-# a1 = []
-# n =27
-# for a in range(len(arr)):
-#     if len(arr)%2 != 0:
-#         if a%2 == 0:
-#             arr[a] += n
-#             a1.append(arr[a])
-#         else:
-#             a1.append(arr[a])
-#     elif len(arr)%2 == 0:
-#         if a%2 != 0:
-#             arr[a] += n
-#             a1.append(arr[a])
-#         else:
-#             a1.append(arr[a])
-    
-    
-    
-     # (짝수항에다가 더한 값들만 출력이 됐어)
-# print(a1) 
-
-'''     
-cipher를 주고 받는데 code번째의 글짜만 암호다
-
-
-
 # a1 = []
 # a = (box[0]//n)
 # b = (box[1]//n)
@@ -119,53 +23,10 @@ cipher를 주고 받는데 code번째의 글짜만 암호다
 # # 가장 긴 변은 sides[-1]인경우
 # # 나머지 변의 경우 sides[0]+sides[-1] > 나머지 변
 # # num = 0
-# lista =[]
-# listb =[]
-# for i in range(0, sides[1]+1): # a = 7부터 11까지 
-#      if sides[0]+i > sides[-1]:
-#           lista.append(i) # 가장 기변의 경우 7개
-# for a in range(i+1, sides[0]+sides[-1]): # 0부터 18까지
-#      if sides[0]+sides[-1] > a:
-#           listb.append(a) # 가장 긴 변이 나머지 인경우 6개
-# answer = len(lista)+len(listb)
-# print(answer)
-
-# n = 3
-# num = ""
-# for a in range(1, n+1): #a = 0,1,2,3
-, false, true, false]
-# temp = []
-# finished[0] = 1
-# finished[1] = 0
-# finished[2] = 1
-# finished[3] = 0
-# # finished[0] = True
-# # answer = []
-# # for i in finished:
-# #      if finished[i] == True:
-# #           answer.append(todo_list.remove(i))
-# # print(answer)
-# prin
-#             lista[a] = lista[b]
-#             lista[b] = temp
-
-# direction = "left"
-# numbers = [4,455,6,4,-1,45,6]
-# # temp = []
-
-# lista = list(my_string)
-# temp = []
-# temp = lista[num1]
-# lista[num1] = lista[num2]
-# lista[num2] = temp
-# print(lista)
-# answer = "".join(lista)
-# print(answer)
-나머지는 다른경우
-(a+b+c)x(a**2+b**2+c**2)
-세 숫자가 모두 같다면
-(a+b+c)x(a**2+b**2+c**2)x(a**3+b**3+c**3)
-'''
+# # lista
+# 세 숫자가 모두 같다면
+# (a+b+c)x(a**2+b**2+c**2)x(a**3+b**3+c**3)
+# '''
 # a =2
 # b =6
 # c= 1
@@ -324,36 +185,7 @@ num의 그 숫자가 있는 자리수를 return하라
 # my_string = "cvsgiorszzzmrpaqpe"
 # index_list = [16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7]
 # # result = "programmers"
-# lista = list(my_string)
-# answer = []
-# for a in index_list:
-#      answer.append(lista[a])
-# print(answer)     
-
-# strArr[0]은 짝수번째 인덱스의 문자열이므로 소문자로 바꿔서 "abc"가 됩니다.
-# strArr[1]은 홀수번째 인덱스의 문자열이므로 대문자로 바꿔서 "ABC"가 됩니다.
-# 따라서 ["abc","ABC"]를 return 합니다.
-# strArr = ["AAA","BBB","CCC","DDD"]
-# result = ["aaa","BBB","ccc","DDD"]
-# answer = []
-# # for i in strArr: 
-# #      answer.append(i.lower())
-# for a in range(len(strArr)):
-#      if a%2 == 0:
-#           answer.append(strArr[a].lower())
-#      elif a%2 != 0:
-#           answer.append(strArr[a].upper)
-# print(answer)
-
-
-# answer = 0
-# for i in range(1, n):
-#      if n < 6:
-#           answer = 0
-#      if (n*i)%6 == 0:
-#           answer = (n*i)//6
-#           break
-# return answer
+# lista = list
 
 # strArr = ["and","notad","abcd"]
 # answer = []
@@ -513,3 +345,58 @@ arr1이 크면 1
 # for a in my_string:
 #      result.append(my_string[a:])
 # print(result)
+
+# .... = h
+# . = e
+# .-.. = l
+# --- = o
+# .--. = p
+# -.-- = y
+# - = t
+# -. = n
+#
+# def solution(letter):
+#     morse = { 
+#     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+#     '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+#     '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+#     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+#     '-.--':'y','--..':'z'
+# }
+#     print(morse['.-'])
+    
+#     letter = letter.split(' ')
+#     answer = []
+#     for i in letter:
+#         answer.append(morse[i])
+#     return ''.join(answer)
+
+'''   
+before에 revsere 한값이 after과 일치하면 1
+안에 요소 순서를 바꾸어서 해당이 안되는 경우
+일치하지 않으면 0
+'''
+# before = "olleh"
+# after = "hello"
+# A = "".join(reversed(before))
+# B = 
+# if before == after:
+#      answer = 1
+# else:
+#      answer = 0
+
+i = 1
+j = 13
+k = 1
+result = 6
+# 정수 i,j,k가 매개 변수일때
+# i부터 j까지 k가 몇번 등장하는 지 return
+# num = 0
+num = []
+for a in range(i, j+1):
+     if str(k) in str(a):
+          num.append(a)
+str(num)
+answer = str(num).count("1")
+print(answer)
+
